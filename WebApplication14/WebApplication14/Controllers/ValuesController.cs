eb7c6556f12f81f0dd4b5cf235400e88ed948d85
ramreim <studen_studen@yahoo.com>
2022-08-21 07:34:55 +0200

    istorijos_test_3

--- WebApplication14/WebApplication14/Controllers/ValuesController.cs
+++ WebApplication14/WebApplication14/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace WebApplication14.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value23" };
+            return new string[] { "value1", "value2355" };
         }
 
         // GET api/values/5
